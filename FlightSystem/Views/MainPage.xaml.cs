bf106e4420c19af659338d85c7a09977aab0b766
Amanbatth295 <124797954+Amanbatth295@users.noreply.github.com>
2024-03-10 20:45:51 -0600

    Update MainPage.xaml.cs

--- FlightSystem/Views/MainPage.xaml.cs
+++ FlightSystem/Views/MainPage.xaml.cs
@@ -1,12 +1,24 @@
-﻿namespace FlightSystem.Views
+using Xamarin.Forms;
+
+namespace FlightSystem.Views
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
         }
-    }
 
+        private void AboutUsButton_Clicked(object sender, EventArgs e)
+        {
+            // Redirect to the About Us tab/page
+            Navigation.PushAsync(new AboutUsPage());
+        }
+
+        private void FindFlightsButton_Clicked(object sender, EventArgs e)
+        {
+            // Redirect to the Find Flights tab/page
+            Navigation.PushAsync(new FindFlightsPage());
+        }
+    }
 }
